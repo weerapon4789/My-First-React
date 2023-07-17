78056f49995e2de8235b9810db836076878978dc
Weerapon <popan.wt@gmail.com>
2023-07-17 23:17:54 +0700

    commit17.07.66

--- src/component/Item.js
+++ src/component/Item.js
@@ -6,8 +6,11 @@ const Item = (props)=> {
     const {title,amount} = props
     const status = amount<0 ? "out":"in"
     const symbol = amount<0 ? "-":"+"
+    const formatNumber=(num)=> {
+        return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+    }
     return(
-        <li className={status}>{title}<span>{symbol}{Math.abs(amount)}</span></li>
+        <li className={status}>{title}<span>{symbol}{formatNumber(Math.abs(amount))}</span></li>
     );
 }
 
--- src/component/ReportComponent.js
+++ src/component/ReportComponent.js
@@ -5,19 +5,24 @@ import './ReportComponent.css';
 const ReportComponent =()=> {
 
     const {income,expence} = useContext(DataContext)
+    const finalTotal = income-expence
+    const statusTotal = finalTotal <0 ? "outF":"inF"
+    const formatNumber=(num)=> {
+        return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+    }
 
     return(
       <div>
         <h3>ยอดเงินคงเหลือ(บาท)</h3>
-        <h1>{income-expence} บาท</h1>
+        <h1 className={statusTotal}>{formatNumber(income-expence)}<span style={{color: "black"}}> บาท</span></h1> 
         <div className="report-container">
             <div>
                 <h4>รายรับทั้งหมด</h4>
-                <p className="report inc">{income}</p>
+                <p className="report inc">{formatNumber(income)}</p>
             </div>
             <div>
                 <h4>รายจ่ายทั้งหมด</h4>
-                <p className="report exp">{expence}</p>
+                <p className="report exp">{formatNumber(expence)}</p>
             </div>
         </div>
       </div>  
